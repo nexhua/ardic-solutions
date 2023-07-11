4e5962dcd7ad0bde143fa3a366dc35991238a2a3
nexhua <CChristofear@hotmail.com>
2023-07-11 04:38:22 +0300

    refactored group discovery logic

--- Four/src/Four.java
+++ Four/src/Four.java
@@ -4,16 +4,15 @@ public class Four {
     public static void main(String[] args) {
         // Initialize board with hardcoded 2D array
 
-        //int[][] cells =  {{1,5,2,9}, {8,7,2,6}, {6,4,3,1}, {9,4,4,7}};
-        //Board board = new Board(cells, cells.length);
+        // int[][] cells =  {{1,5,2,9}, {8,7,2,6}, {6,4,3,1}, {9,4,4,7}};
+        // Board board = new Board(cells, cells.length);
 
         // Randomly initialized board
-
         Board board = new Board(10);
 
         board.printBoard();
 
-        System.out.println("\nGroup count: " + board.checkBoard());
+        System.out.println("\nGroup count: " + board.check());
 
         System.out.println("\nEach group and cells in that group:");
         board.printGroups();
--- Four/src/com/ardic/four/Board.java
+++ Four/src/com/ardic/four/Board.java
@@ -1,7 +1,6 @@
 package com.ardic.four;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class Board {
 
@@ -21,12 +20,12 @@ public class Board {
         this.createBoard();
     }
 
-    public Board (int[][] cells, int size) {
+    public Board(int[][] cells, int size) {
         this.N = size;
         this.Board = new Cell[this.N][this.N];
 
-        for(int i=0;i<size;i++) {
-            for(int j=0;j<size;j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 Cell cell = new Cell(cells[i][j], i, j);
                 this.Board[i][j] = cell;
             }
@@ -36,8 +35,8 @@ public class Board {
     private void createBoard() {
         Random random = new Random();
         this.Board = new Cell[this.N][this.N];
-        for(int i=0;i<this.N;i++) {
-            for(int j=0;j<this.N;j++) {
+        for (int i = 0; i < this.N; i++) {
+            for (int j = 0; j < this.N; j++) {
                 Cell cell = new Cell(random.nextInt(1, 10), i, j);
                 this.Board[i][j] = cell;
             }
@@ -45,16 +44,16 @@ public class Board {
     }
 
     public int checkBoard() {
-        for(int i=0;i<this.N;i++) {
-            for(int j=0;j<this.N;j++) {
+        for (int i = 0; i < this.N; i++) {
+            for (int j = 0; j < this.N; j++) {
                 Cell curr = this.Board[i][j];
 
                 Cell right = tryGetCell(i, j + 1);
                 Cell left = tryGetCell(i, j - 1);
-                Cell up = tryGetCell(i - 1,j);
+                Cell up = tryGetCell(i - 1, j);
                 Cell down = tryGetCell(i + 1, j);
 
-                Cell[] neighbours = new Cell[]{up,down,left,right};
+                Cell[] neighbours = new Cell[]{up, down, left, right};
 
                 compareNeighbours(curr, neighbours);
 
@@ -64,16 +63,89 @@ public class Board {
         return this.groups.size();
     }
 
+    public int check() {
+        for (int i = 0; i < this.N; i++) {
+            for (int j = 0; j < this.N; j++) {
+                Cell curr = this.Board[i][j];
+
+                if(curr.getGroup() != null) {
+                    continue;
+                }
+
+                Cell right = tryGetCell(i, j + 1);
+                Cell left = tryGetCell(i, j - 1);
+                Cell up = tryGetCell(i - 1, j);
+                Cell down = tryGetCell(i + 1, j);
+
+                Cell[] neighbours = new Cell[]{up, down, left, right};
+
+                compare(curr, neighbours);
+
+            }
+        }
+
+        return this.groups.size();
+    }
+
+
+    private void compare(Cell curr, Cell[] neighbours) {
+        List<Cell> neighbourMatches = filterNeighbours(curr, neighbours);
+
+        // If we have a match
+        if (neighbourMatches.size() > 0) {
+            Group group = new Group();
+            this.groups.add(group);
+
+            searchGroup(curr, neighbourMatches, group);
+        }
+
+    }
+
+    private void searchGroup(Cell curr, List<Cell> neighbours, Group group) {
+        // Add current cell to the group. Being in a group also means visited.
+        group.add(curr);
+
+        // All adjacent matching neighbours are added to the queue
+        Queue<Cell> queue = new LinkedList<>(neighbours);
+
+
+        while (!queue.isEmpty()) {
+            Cell cell = queue.poll();
+
+            if (cell.getGroup() == null) {
+                group.add(cell);
+                queue.addAll(filterNeighbours(cell, getNeighbours(cell)));
+            }
+
+        }
+    }
+
+    private List<Cell> filterNeighbours(Cell curr, Cell[] neighbours) {
+        return Arrays.stream(neighbours).filter(cell -> cell != null && cell.getValue() == curr.getValue()).toList();
+    }
+
+    private Cell[] getNeighbours(Cell center) {
+        int i = center.getI();
+        int j = center.getJ();
+
+        Cell right = tryGetCell(i, j + 1);
+        Cell left = tryGetCell(i, j - 1);
+        Cell up = tryGetCell(i - 1, j);
+        Cell down = tryGetCell(i + 1, j);
+
+        return new Cell[]{up, down, left, right};
+    }
+
     public void compareNeighbours(Cell curr, Cell[] neighbours) {
 
-        for(int i=0;i<neighbours.length;i++) {
-            if(neighbours[i] != null) {
+        for (int i = 0; i < neighbours.length; i++) {
+            if (neighbours[i] != null) {
                 Cell neighbour = neighbours[i];
                 // If they have the same value
-                if(curr.getValue() == neighbour.getValue()) {
+                if (curr.getValue() == neighbour.getValue()) {
 
                     // If both of the cells are not in a group
-                    if(curr.getGroup() == null && neighbour.getGroup() == null) {
+                    if (curr.getGroup() == null && neighbour.getGroup() == null) {
                         Group group = new Group();
 
                         group.add(curr);
@@ -81,12 +153,11 @@ public class Board {
 
                         this.groups.add(group);
                     } // If only one of the cell is in a group
-                    else if(curr.getGroup() == null ^ neighbour.getGroup() == null)  {
-                        if(curr.getGroup() != null) {
+                    else if (curr.getGroup() == null ^ neighbour.getGroup() == null) {
+                        if (curr.getGroup() != null) {
                             Group group = curr.getGroup();
                             group.add(neighbour);
-                        }
-                        else {
+                        } else {
                             Group group = neighbour.getGroup();
                             group.add(curr);
                         }
@@ -94,11 +165,11 @@ public class Board {
                     } // If both of the cells are in a group
                     else {
                         // If both cells in a group but the group is different, merge groups
-                        if(curr.getGroup() != neighbour.getGroup()) {
+                        if (curr.getGroup() != neighbour.getGroup()) {
                             int currGroupIndex = this.groups.indexOf(curr.getGroup());
                             int neighbourGroupIndex = this.groups.indexOf(neighbour.getGroup());
 
-                            if(currGroupIndex != -1 && neighbourGroupIndex != -1) {
+                            if (currGroupIndex != -1 && neighbourGroupIndex != -1) {
                                 Group neighbourGroup = neighbour.getGroup();
                                 curr.getGroup().add(neighbourGroup.getCells());
                                 this.groups.remove(neighbourGroupIndex);
@@ -115,17 +186,16 @@ public class Board {
 
 
     public Cell tryGetCell(int i, int j) {
-        if((i >= 0 && i < this.N) && (j >= 0 && j < this.N)) {
+        if ((i >= 0 && i < this.N) && (j >= 0 && j < this.N)) {
             return this.Board[i][j];
-        }
-        else {
+        } else {
             return null;
         }
     }
 
     public void printBoard() {
-        for(int i=0;i<this.N;i++) {
-            for(int j=0;j<this.N;j++) {
+        for (int i = 0; i < this.N; i++) {
+            for (int j = 0; j < this.N; j++) {
                 System.out.format("%d ", this.Board[i][j].getValue());
             }
             System.out.println();
@@ -133,7 +203,7 @@ public class Board {
     }
 
     public void printGroups() {
-        for(Group group : this.getGroups()) {
+        for (Group group : this.getGroups()) {
             System.out.println(group.toString());
         }
     }
